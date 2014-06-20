09673e5c74f5bae962fc087d528cb48bf252f481
André Freitas <p.andrefreitas@gmail.com>
2014-06-20 21:45:09 +0100

    Já mostra os candidatos

--- app.js
+++ app.js
@@ -1,70 +1,88 @@
 //Define an angular module for our app
 var sampleApp = angular.module('sampleApp', []);
- 
+
+sampleApp.factory('candidatsService', function($rootScope, $http) {
+    var candidatsService = {};
+
+    candidatsService.data = {};
+
+    candidatsService.get = function() {
+        $http.get('candidats.json')
+            .success(function(data) {
+                candidatsService.data.candidats = data;
+            });
+
+        return candidatsService.data;
+    };
+
+    return candidatsService;
+});
+
+
 //Define Routing for app
 sampleApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/candidatos', {
-        templateUrl: 'templates/candidatos.html',
-        controller: 'CandidatosController'
-    }).
-      when('/candidatos-suplentes', {
-        templateUrl: 'templates/candidatos-suplentes.html',
-        controller: 'CandidatosSuplentesController'
-    }).
-      when('/proposta', {
-        templateUrl: 'templates/proposta.html',
-        controller: 'PropostaController'
-      }).
-      when('/apresentacao', {
-        templateUrl: 'templates/apresentacao.html',
-        controller: 'ApresentacaoController'
-      }).
-      when('/info', {
-        templateUrl: 'templates/info.html',
-        controller: 'InfoController'
-      }).
-      otherwise({
-        redirectTo: '/proposta'
-      });
-}]);
- 
- 
-sampleApp.controller('CandidatosController', function($scope) {
-     
-    $scope.message = 'Candidatos';
-     
+                  function($routeProvider) {
+                      $routeProvider.
+                      when('/candidatos', {
+                          templateUrl: 'templates/candidatos.html',
+                          controller: 'CandidatosController'
+                      }).
+                      when('/candidatos-suplentes', {
+                          templateUrl: 'templates/candidatos-suplentes.html',
+                          controller: 'CandidatosSuplentesController'
+                      }).
+                      when('/proposta', {
+                          templateUrl: 'templates/proposta.html',
+                          controller: 'PropostaController'
+                      }).
+                      when('/apresentacao', {
+                          templateUrl: 'templates/apresentacao.html',
+                          controller: 'ApresentacaoController'
+                      }).
+                      when('/info', {
+                          templateUrl: 'templates/info.html',
+                          controller: 'InfoController'
+                      }).
+                      otherwise({
+                          redirectTo: '/proposta'
+                      });
+                  }]);
+
+
+sampleApp.controller('CandidatosController', function($scope, $http, candidatsService) {
+    $scope.candidats = candidatsService.get();
+
 });
- 
-sampleApp.controller('CandidatosSuplentesController', function($scope) {
-     
-    $scope.message = 'Candidatos';
-     
+
+sampleApp.controller('CandidatosSuplentesController', function($scope, $http, candidatsService) {
+   $scope.candidats = candidatsService.get();
+
 });
- 
+
 sampleApp.controller('PropostaController', function($scope) {
- 
+
     $scope.message = 'Proposta';
- 
+
 });
 
 sampleApp.controller('ApresentacaoController', function($scope) {
- 
+
     $scope.message = 'Apresentação';
- 
+
 });
 
 sampleApp.controller('InfoController', function($scope) {
- 
+
     $scope.message = 'Info';
- 
+
 });
 
 sampleApp.controller('MainCtrl', function($scope, $location, $anchorScroll) {
-  $scope.scrollTo = function(id) {
-    $location.hash(id);
-    console.log($location.hash());
-    $anchorScroll();
-  };
+    $scope.scrollTo = function(id) {
+        $location.hash(id);
+        console.log($location.hash());
+        $anchorScroll();
+    };
 });
+
+
